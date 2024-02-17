1626c868efd2c0718430b180da311ad44b101295
Lakshay Jain <72689642+CodesbyLakshay@users.noreply.github.com>
2024-02-17 19:15:04 +0000

    Find All Duplicates in an Array

--- Medium/442 Find All Duplicates in an Array.java
+++ Medium/442 Find All Duplicates in an Array.java
@@ -1,5 +1,5 @@
-public class Solution {
-    public List<Integer> findDisappearedNumbers(int[] nums) {
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
         int index =0;
         while(index< nums.length){
             int correct = nums[index]-1;
@@ -13,15 +13,15 @@ public class Solution {
          List<Integer> l = new ArrayList<>();
        for (int i = 0; i < nums.length; i++) {
            if(nums[i]!=i+1){
-               l.add(i+1);
+               l.add(nums[i]);
            }
-           
-       }
-return l;
     }
-    static void swap(int[] arr,int one , int second) {
+    return l;
+}
+static void swap(int[] arr,int one , int second) {
         int temp = arr[one];
         arr[one] = arr[second];
         arr[second] = temp;
     }
+
 }
